2b28397a305ba1789ef9775a778e36d943d67abf
brennerm <xamrennerb@gmail.com>
2019-09-06 19:08:38 +0200

    Updates

---
+++ precache-manifest.cb60787e1786db8c8da767a92ae08296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcf87b6b3dd0d82ae1b0b902a4a686c",
+    "url": "/pi-power-calc/index.html"
+  },
+  {
+    "revision": "889cbe515596bdca12b9",
+    "url": "/pi-power-calc/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0da5a579513496da7039",
+    "url": "/pi-power-calc/static/css/main.0ff44bea.chunk.css"
+  },
+  {
+    "revision": "889cbe515596bdca12b9",
+    "url": "/pi-power-calc/static/js/2.6099eccc.chunk.js"
+  },
+  {
+    "revision": "0da5a579513496da7039",
+    "url": "/pi-power-calc/static/js/main.7dc1a1c8.chunk.js"
+  },
+  {
+    "revision": "f40c6929fa6dc91088d9",
+    "url": "/pi-power-calc/static/js/runtime~main.83ca38b2.js"
+  }
+]);
