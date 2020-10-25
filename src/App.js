bde35c69d52f9f4bbd32c8c993836153827e73c3
Max Brenner <xamrennerb@gmail.com>
2020-10-25 16:03:37 +0100

    make cost calculation currency independent

--- src/App.js
+++ src/App.js
@@ -79,7 +79,6 @@ class App extends Component {
 
     this.state = {
       'cost': '',
-      'currency': 'USD',
       'interval': this.time_intervals.yearly,
       'kwh': '',
       'kwh_price': '',
@@ -87,70 +86,54 @@ class App extends Component {
       'model': ''
     }
 
-    this.calcCost = this.calcCost.bind(this);
-    this.currencyChange = this.currencyChange.bind(this);
+    this.calc = this.calc.bind(this);
     this.intervalChange = this.intervalChange.bind(this);
     this.loadChange = this.loadChange.bind(this);
     this.modelChange = this.modelChange.bind(this);
     this.priceChange = this.priceChange.bind(this);
   }
 
-  componentDidMount() {
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
-      .then(response => response.json())
-      .then(data => {this.exchange_rates = data})
-  }
-
-  currencyChange(event) {
-    let value = event.target.value;
-    this.setState({'currency': value}, function() {
-      this.calcCost();
-    });
-  }
-
   intervalChange(event) {
     let value = parseInt(event.target.value, 10);
-    this.setState({'interval': value}, function() {
-      this.calcCost();
+    this.setState({ 'interval': value }, function () {
+      this.calc();
     });
   }
 
   loadChange(event) {
     let value = parseFloat(event.target.value);
-    this.setState({'load': value}, function() {
-      this.calcCost();
+    this.setState({ 'load': value }, function () {
+      this.calc();
     });
   }
 
   modelChange(event) {
     let value = event.target.value;
-    this.setState({'model': value}, function() {
-      this.calcCost();
+    this.setState({ 'model': value }, function () {
+      this.calc();
     });
   }
 
   priceChange(event) {
     let value = parseFloat(event.target.value);
-    this.setState({'kwh_price': value}, function() {
-      this.calcCost();
+    this.setState({ 'kwh_price': value }, function () {
+      this.calc();
     });
   }
 
-  calcCost() {
+  calc() {
     if (this.state.model === '') {
       return;
     }
     let model = this.pi_models[this.state.model]
-    let currency_factor = this.exchange_rates.rates[this.state.currency]
     let power_con = model.power_con_min + ((model.power_con_max - model.power_con_min) * this.state.load)
     let kwh = power_con / 1000
     let total_kwh = kwh * this.state.interval
-    this.setState({'kwh': total_kwh.toFixed(4)});
+    this.setState({ 'kwh': total_kwh.toFixed(4) });
 
-    let cost = this.state.kwh_price * total_kwh / 100
-    cost = cost * currency_factor
-    cost = cost.toFixed(4);
-    this.setState({'cost': cost});
+    let cost = this.state.kwh_price * total_kwh || 0
+    cost = cost.toFixed(2);
+    this.setState({ 'cost': cost });
   }
 
   render() {
@@ -160,74 +143,68 @@ class App extends Component {
           <h1 className="App-title">Raspberry Pi Power Calculator</h1>
         </header>
         <form className="container">
-        <div className="row form-group">
-        <select className="form-control" value={this.state.model} onChange={this.modelChange}>
-          <option value='' disabled>Select your Model</option>
-          {
-            Object.keys(this.pi_models).map((model_key, index) => (
-              <option key={model_key} value={model_key}>
-              {this.pi_models[model_key].name} (~{this.pi_models[model_key].power_con_min}–{this.pi_models[model_key].power_con_max} W)
-              </option>
-            ))
-          }
-        </select>
-        </div>
-        
-        <div className="row form-group">
-        <label className="form-check-label">Time Range</label>
-        {
-          Object.keys(this.time_intervals).map((interval, index) => (
-            <div key={interval} className="form-check form-check-inline">
-            <input type="radio" className="form-check-input" name="interval" value={this.time_intervals[interval]} onChange={this.intervalChange} checked={this.state.interval === this.time_intervals[interval]}/>
-            <label className="form-check-label">{interval}</label>
-            </div>
-          ))
-        }
-        </div>
-
-        <div className="row form-group">
-        <label className="form-check-label">Load</label>
-        {
-          Object.keys(this.load).map((load, index) => (
-            <div key={load} className="form-check form-check-inline">
-            <input type="radio" className="form-check-input" name="load" value={this.load[load]} onChange={this.loadChange} checked={this.state.load === this.load[load]}/>
-            <label className="form-check-label">{load}</label>
-            </div>
-          ))
-        }
-        </div>
-
-        <div className="row form-group">
-        <div className="input-group">
-        <input type="number" min="0" step="0.01" className="form-control" placeholder="0.00" value={this.state.kwh_price} onChange={this.priceChange}/>
-          <div className="input-group-append">
-            <span className="input-group-text" id="basic-addon2">ct/kWh</span>
+          <div className="row form-group">
+            <select className="form-control" value={this.state.model} onChange={this.modelChange}>
+              <option value='' disabled>Select your Model</option>
+              {
+                Object.keys(this.pi_models).map((model_key, index) => (
+                  <option key={model_key} value={model_key}>
+                    {this.pi_models[model_key].name} (~{this.pi_models[model_key].power_con_min}–{this.pi_models[model_key].power_con_max} W)
+                  </option>
+                ))
+              }
+            </select>
           </div>
-        </div>
-        </div>
-        
-        <div className="row form-group">
-          <div className="input-group">
-          <input type="text" className="form-control" placeholder="0.0000" value={this.state.kwh} readOnly/>
-            <div className="input-group-append">
-              <span className="input-group-text">kWh</span>
-            </div>
-          </div>
-        </div>
 
-        <div className="row form-group">
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="0.0000" value={this.state.cost} readOnly/>
+          <div className="row form-group">
+            <label className="form-check-label">Time Range:</label>
+            {
+              Object.keys(this.time_intervals).map((interval, index) => (
+                <div key={interval} className="form-check form-check-inline">
+                  <input type="radio" className="form-check-input" name="interval" value={this.time_intervals[interval]} onChange={this.intervalChange} checked={this.state.interval === this.time_intervals[interval]} />
+                  <label className="form-check-label">{interval}</label>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className="row form-group">
+            <label className="form-check-label">Load:</label>
+            {
+              Object.keys(this.load).map((load, index) => (
+                <div key={load} className="form-check form-check-inline">
+                  <input type="radio" className="form-check-input" name="load" value={this.load[load]} onChange={this.loadChange} checked={this.state.load === this.load[load]} />
+                  <label className="form-check-label">{load}</label>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className="row form-group">
+            <div className="input-group">
+              <input type="number" min="0" step="0.01" className="form-control" placeholder="0.00" value={this.state.kwh_price} onChange={this.priceChange} />
               <div className="input-group-append">
-                <span className="input-group-addon">
-                <select className="form-control" value={this.state.currency} onChange={this.currencyChange}>
-                    <option value="USD">$</option>
-                    <option value="EUR">€</option>
-                </select>
-                </span>
+                <span className="input-group-text" id="basic-addon2">price/kWh</span>
               </div>
+            </div>
           </div>
-        </div>
+
+          <div className="row form-group">
+            <label className="form-check-label">Total consumption:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="0.0000" value={this.state.kwh} readOnly />
+              <div className="input-group-append">
+                <span className="input-group-text">kWh</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="row form-group">
+            <label className="form-check-label">Total costs:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="0.0000" value={this.state.cost} readOnly />
+            </div>
+          </div>
 
         </form>
       </div>
