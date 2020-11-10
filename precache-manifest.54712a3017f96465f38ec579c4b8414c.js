d7a5bdcfa84985c8c586758126e4e0b1afbb0b53
brennerm <xamrennerb@gmail.com>
2020-11-10 23:11:09 +0000

    Deploying to gh-pages from  @ ffa5feb3cdc06b5e6915e7f23a868f1b778990f0 🚀

--- precache-manifest.54712a3017f96465f38ec579c4b8414c.js
+++ precache-manifest.54712a3017f96465f38ec579c4b8414c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f4c2bd61fcc49e363b329a9b6b990359",
+    "revision": "3cb83e3a8b63d6a7a91516206cd5d515",
     "url": "/pi-power-calc/index.html"
   },
   {
-    "revision": "ddb1ac024e8145abd703",
+    "revision": "4c4a5662710aafc80355",
     "url": "/pi-power-calc/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "f1f973e9017856ba099e",
+    "revision": "19150976668a988698d8",
     "url": "/pi-power-calc/static/css/main.56de6686.chunk.css"
   },
   {
-    "revision": "ddb1ac024e8145abd703",
-    "url": "/pi-power-calc/static/js/2.b17f4c0e.chunk.js"
+    "revision": "4c4a5662710aafc80355",
+    "url": "/pi-power-calc/static/js/2.b91cfe82.chunk.js"
   },
   {
-    "revision": "f1f973e9017856ba099e",
-    "url": "/pi-power-calc/static/js/main.d1a0ce20.chunk.js"
+    "revision": "19150976668a988698d8",
+    "url": "/pi-power-calc/static/js/main.0b3030cb.chunk.js"
   },
   {
     "revision": "f40c6929fa6dc91088d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pi-power-calc/precache-manifest.e586f44848c186cc959c2ee7585c7484.js"
+  "/pi-power-calc/precache-manifest.54712a3017f96465f38ec579c4b8414c.js"
 );
 
 self.addEventListener('message', (event) => {
