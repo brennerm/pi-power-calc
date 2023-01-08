bdce7bc82cf7cf23c86320ee553e5d3efe32a37a
brennerm <xamrennerb@gmail.com>
2023-01-08 16:28:47 +0000

    Deploying to gh-pages from  @ bdb6409405c6c2e6d97b6039c0aa9e0119111449 🚀

---
+++ precache-manifest.2f953f70d3ae6684a87f715f022b6c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8985c5cf5da09bf505c0d6819d86dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86908bb46efd97ab3846",
+    "url": "/static/css/2.dc6abeed.chunk.css"
+  },
+  {
+    "revision": "f40ebdf5659d230eebf0",
+    "url": "/static/css/main.56de6686.chunk.css"
+  },
+  {
+    "revision": "86908bb46efd97ab3846",
+    "url": "/static/js/2.45697d19.chunk.js"
+  },
+  {
+    "revision": "b21c0b070fbf389a149e773557d68a8a",
+    "url": "/static/js/2.45697d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40ebdf5659d230eebf0",
+    "url": "/static/js/main.aaf1c5f7.chunk.js"
+  },
+  {
+    "revision": "b99486c16e85cd8fcb52",
+    "url": "/static/js/runtime-main.a5a5802b.js"
+  }
+]);
